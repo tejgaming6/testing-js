26d6afdce4703cb73ebf7a5dabf8c9a2e37cbdb4
tejgaming6 <94366226+tejgaming6@users.noreply.github.com>
2023-10-05 20:58:25 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,8 @@
+const accountId = 2161;
+let accountEmail = "loldon'tread@gmail.com"
+var dontusethis = "dont use var, forget about it"
+let undefinied; // will be undeifined 
+itwillgetprinted = "it will get printed without using let or const but don't use it"
+console.log(accountId);
+
+console.table([ accountId, accountEmail, dontusethis]);
---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,11 @@
+// number => 2 to power 53
+// biginit used in trading or big server like reddit where they have to use bigger numbers
+// blloean = true/false
+// null = stand alone value // which type is object 
+// undefinied = undefinied 
+// symbol => unique it helps us recognise faster & easier 
+// console.log(typeof ""); = string
+// console.log(typeof 25); = number 
+// console.log(typeof null); = object
+// console.log(typeof undefinied); = unefinied
+// "" - prefered at string
