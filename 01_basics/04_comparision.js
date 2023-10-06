f39138343de24e99ffa1863309a71d3f3f531db8
tejgaming6 <94366226+tejgaming6@users.noreply.github.com>
2023-10-06 22:37:48 +0000

    04 push

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,23 @@
+let score = "tej"
+
+console.log(typeof score);
+console.log(typeof (score));
+
+let valueInNumber = Number(score);
+console.log(valueInNumber);
+console.log(typeof valueInNumber );
+// Tej = error
+// "33" => 33
+// "33abc" => NaN
+// undefinied = NaN
+// true => 1, false => 0
+// null => 0
+
+// empty strings => false
+// include anyting in beteen strings => true 
+
+let someNumber = 33
+
+let stringNumber = String(someNumber);
+console.log(someNumber);
+console.log(typeof stringNumber);
