fecb3b4d35f8c615e462d6c684e969aa6737d110
tejgaming6 <94366226+tejgaming6@users.noreply.github.com>
2023-10-07 17:08:37 +0000

    datatypes

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,64 @@
+// primitive or non primitive 
+// call by value or call by reference 
+// Primitive 7 categories 
+// type : String Number, Boolean, null , undefined, Symbol, Bigint 
+// reference / non premitive 
+// direactly allowed 
+// Array, Objects Functions 
+
+//js language is static or dynamic?
+//const bigNumber = 4528747835647865784n
+// Bigint
+
+// non premitive / reference 
+
+// const heros = ["spiderman", "Ironman", "Thor", "Hulk"];
+// let details = {
+//     name: "tej",
+//     age: "will not reveal"
+// }
+
+// const myfunction = function () {
+//     console.log("bye");
+// }
+
+// console.log(typeof );
+
+// // https://262.ecma-international.org/5.1/#sec-11.4.3
+
+// data type summary 
+
+// const stringChecking = "it's a string"; //string
+// const numberChecking = 25; // number
+// const boleanChecking = true; // boolean
+// const nullchecking = null; // object 
+// const unefinedChecking = undefined; // undefind 
+// let undefichecking; // undefined
+// let symbolChecking = Symbol('i24') // symbol
+// let symChecking = Symbol('124') // symbol
+// let biginitChecking = 92947826329864923649n; // biginit
+// console.log(typeof stringChecking); 
+// console.log(typeof numberChecking);
+// console.log(typeof boleanChecking);
+// console.log(typeof nullchecking);
+// console.log(typeof unefinedChecking);
+// console.log(typeof undefichecking);
+// console.log(typeof symbolChecking);
+// console.log(typeof symChecking);
+// console.log(typeof biginitChecking);
+// console.log(symbolChecking === symChecking); // false 
+
+// non premitive / reference
+
+const heros = ["spiderman", "Ironman", "Thor", "Hulk"];
+let details = {
+    name: "tej",
+    age: "will not reveal"
+}
+
+const myfunction = function(){
+    console.log("bye");
+}
+// console.log(typeof myfunction); // function but said Function Object 
+// console.log(typeof details);// Object
+// console.log(typeof heros); // Object
