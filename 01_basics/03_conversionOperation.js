673d9365583a8d8664a698d5f556a9dc924ebbe8
tejgaming6 <94366226+tejgaming6@users.noreply.github.com>
2023-10-07 15:24:18 +0000

    changes

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,11 +1,60 @@
-// number => 2 to power 53
-// biginit used in trading or big server like reddit where they have to use bigger numbers
-// blloean = true/false
-// null = stand alone value // which type is object 
-// undefinied = undefinied 
-// symbol => unique it helps us recognise faster & easier 
-// console.log(typeof ""); = string
-// console.log(typeof 25); = number 
-// console.log(typeof null); = object
-// console.log(typeof undefinied); = unefinied
-// "" - prefered at string
+// // number => 2 to power 53
+// // biginit used in trading or big server like reddit where they have to use bigger numbers
+// // blloean = true/false
+// // null = stand alone value // which type is object 
+// // undefinied = undefinied 
+// // symbol => unique it helps us recognise faster & easier 
+// // console.log(typeof ""); = string
+// // console.log(typeof 25); = number 
+// // console.log(typeof null); = object
+// // console.log(typeof undefinied); = unefinied
+// // "" - prefered at string
+
+// let score = "tej"
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+// let valueInNumber = Number(score);
+// console.log(valueInNumber);
+// console.log(typeof valueInNumber );
+// Tej = error
+// "33" => 33
+// "33abc" => NaN
+// undefinied = NaN
+// true => 1, false => 0
+// null => 0
+
+// empty strings => false
+// include anyting in beteen strings => true 
+
+// let someNumber = 33
+
+// let stringNumber = String(someNumber);
+// console.log(someNumber);
+// console.log(typeof stringNumber);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 3 + 4);
+// console.log(1 + 2 + 3 + "4");
+// console.log( 3 + 4 * 5 % 2);
+// console.log( (3 + 4) * 5 % 2);
+// console.log( (7 * 5) % 2);
+// console.log( 35 % 2);
+
+console.log(true);
+console.log(+true); // all wrong practice 
+// console.log(true+);
+// num1 = num2 = num3 = 2 + 2
+// pre fix 
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter);
+// post fix
+let gameCounters = 110
+++gameCounters;
+console.log(gameCounters);
+
+// link to study 
+// // https://tc39.es/ecma262/multipage/abstract-operations.html#sec-type-conversion
