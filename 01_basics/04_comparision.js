ea203a0b6a3e15abf5de28fb729e748ea7fd955f
tejgaming6 <94366226+tejgaming6@users.noreply.github.com>
2023-10-07 16:23:47 +0000

    push

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,14 @@
+// always ensure to compare same data types 
+// console.log("2" > 1);
+// console.log("05" > 1);
+// console.log(null > 1);
+// console.log(null > 0);
+// console.log(null >= 0);
+
+// comparition & equality is different
+
+// === values checks stritly also checks data type 
+
+// use same data type
+
+// clean code
