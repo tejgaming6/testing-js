c6c35b77c6548485bfb914b3e2c3cf938124041f
tejgaming6 <94366226+tejgaming6@users.noreply.github.com>
2023-10-07 16:12:12 +0000

    03

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -43,18 +43,32 @@
 // console.log( (7 * 5) % 2);
 // console.log( 35 % 2);
 
-console.log(true);
-console.log(+true); // all wrong practice 
+// console.log(true);
+// console.log(+true); // all wrong practice 
 // console.log(true+);
 // num1 = num2 = num3 = 2 + 2
 // pre fix 
-let gameCounter = 100
-gameCounter++;
-console.log(gameCounter);
-// post fix
-let gameCounters = 110
-++gameCounters;
-console.log(gameCounters);
+// let gameCounter = 100
+// gameCounter++;
+// console.log(gameCounter);
+// // post fix
+// let gameCounters = 110
+// ++gameCounters;
+// console.log(gameCounters);
 
 // link to study 
 // // https://tc39.es/ecma262/multipage/abstract-operations.html#sec-type-conversion
+
+let x = 3;
+const y = x++;
+// console.log(x);
+// console.log(y);
+
+//postfix upper number gets increased by 1 x++ adds one digit upper 
+
+let a = 3;
+const b = ++a;
+console.log(a);
+console.log(b);
+
+// prefix ex both number gets 1 increased 
