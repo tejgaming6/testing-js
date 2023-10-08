87d3e9a7cd2b6953975d74fbf0d52fe2f3235857
tejgaming6 <94366226+tejgaming6@users.noreply.github.com>
2023-10-08 20:52:24 +0000

    strings

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,9 @@
+const name = "tej"
+const gamesPlayed = 15
+
+// console.log(`hello my name is ${name} i have played ${gamesPlayed} games, i love playing games`);
+
+const gameName = String('counterStrike');
+
+console.log(gameName);
+
