1bd55e979c270f843467edbb6f4837b3d335d592
tejgaming6 <94366226+tejgaming6@users.noreply.github.com>
2023-10-08 18:05:36 +0000

    push

---
+++ 01_basics/06_nums_and_math.js
@@ -0,0 +1,30 @@
+const score = 200;
+
+// console.log(score);
+
+const balance = new Number(50);
+// console.log(balance);
+
+// console.log(score.toString().length);
+// console.log(score.toFixed(2));
+
+const exampleNumber = 89.6323
+
+// console.log(exampleNumber.toPrecision(2));
+
+// const thoudsands = 100000000
+// console.log(thoudsands.toLocaleString('en-IN'));
+
+// ____________________Maths_____________________________ maths lib by default with js 
+
+// console.log(Math);
+// console.log(Math.abs(-6)); // make it positive 6 doesn't make it negtive
+
+// console.log(Math.round(3.4)); //it rounds up stuff 0.5 basics
+// console.log(Math.floor(2.5)); // floor make it lower number
+// console.log(Math.ceil(2.1));// it makes the upper number come if it cross any number it will show upper number example 2.1 it will print 3
+//  console.log(Math.min(4,7,8,9,));
+//  console.log(Math.max(4,7,8,9,));
+
+ console.log(Math.random());
+ console.log(Math.floor(Math.random( 1 + 1)* 10));
