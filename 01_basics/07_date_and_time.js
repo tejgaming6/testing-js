74430e8563e2f6524fe110291b080d634ae01909
tejgaming6 <94366226+tejgaming6@users.noreply.github.com>
2023-10-09 21:06:05 +0000

    date_and_time

---
+++ 01_basics/07_date_and_time.js
@@ -0,0 +1,18 @@
+// // let myDay = new Date();
+// // console.log(myDay.toTimeString());
+// // console.log(myDay.toDateString());
+// // console.log(myDay.toLocaleString());
+// // console.log(myDay.toLocaleDateString());
+// // console.log(typeof myDay);
+
+// // let configSpecificDate = new Date("29-0-2023");
+// // console.log(configSpecificDate.toLocaleString());
+
+// // let timeStrap = Date.now();
+
+
+// let createdDate = new Date("05-09-2020");
+// console.log(Math.floor(createdDate.getTime() / 1000));
+
+// let newDate = new Date();
+// console.log(newDate.getDay());
